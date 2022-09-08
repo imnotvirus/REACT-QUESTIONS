8ea35be2cc0ab82746a376cd75288237e87e880c
Luiz Claudio <lcluizclaudio96@gmail.com>
2022-09-08 17:02:11 -0300

    Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,30 +1,120 @@
-import { useState } from "react";
-import questions from '../../utils/questions.json'
-import React from "react";
-import { Container, Label } from "./styles";
+import React, { useCallback, useState } from 'react';
+import { BsCheckLg } from 'react-icons/bs';
+import { FaChevronUp } from 'react-icons/fa';
+import { HiOutlineClipboardList } from 'react-icons/hi';
+import Question, { IQuestion } from '../../components/Question';
+import theme from '../../styles/theme';
+import questions from '../../utils/questions.json';
+import {
+	Container,
+	ContainerCheckLabel,
+	ContainerIcon,
+	ContainerLabels,
+	Header,
+	HeaderQuestion,
+	Label,
+	LabelText,
+	TitleGroup,
+	TitleQuestion,
+	ToggleButton,
+} from './styles';
 
-const Home: React.FC = () => {
-  const [count, setCount] = useState(0);
-
-  return (
-    <Container>
-      <header>
-        <strong>Acentuação Gráfica</strong>
-        <button>^</button>
-      </header>
-      <section>
-        <header>
-          <i>iconsss</i>
-          <span>Uso dos Porquês</span>
-        </header>
-        {
-          questions.map((item, index) => (
-            <Label>exercício {index + 1}</Label>
-          ))
-        }
-      </section>
-    </Container>
-  );
+export interface answeredQuestionsProps {
+	questionId: string;
+	answerId: string;
 }
+const Home: React.FC = () => {
+	const [answeredQuestions, setAnsweredQuestions] = useState<
+		answeredQuestionsProps[]
+	>([]);
+	const [selectedQuestion, setSelectedQuestion] = useState<IQuestion>();
+	const handleMarkQuestion = (response: answeredQuestionsProps) => {
+		setAnsweredQuestions((old) => [
+			...old.filter((item) => item.questionId !== response.questionId),
+			response,
+		]);
+	};
+	const handlePreviousQuestion = useCallback(() => {
+		if (selectedQuestion) {
+			setSelectedQuestion(
+				questions[
+					questions.findIndex((item) => item.id === selectedQuestion.id) - 1
+				]
+			);
+		}
+	}, [selectedQuestion]);
+	const handleNextQuestion = useCallback(() => {
+		if (selectedQuestion) {
+			setSelectedQuestion(
+				questions[
+					questions.findIndex((item) => item.id === selectedQuestion.id) + 1
+				]
+			);
+		}
+	}, [selectedQuestion]);
+
+	const [show, setShow] = useState(true);
+	return (
+		<Container>
+			<Header>
+				<TitleGroup>Acentuação Gráfica</TitleGroup>
+				<ToggleButton rotate={show} onClick={() => setShow((old) => !old)}>
+					<FaChevronUp fill="white" />
+				</ToggleButton>
+			</Header>
+			<section>
+				<HeaderQuestion>
+					<ContainerIcon>
+						<HiOutlineClipboardList size={20} stroke={theme.COLORS.GRAY[100]} />
+					</ContainerIcon>
+					<TitleQuestion>Uso dos Porquês</TitleQuestion>
+				</HeaderQuestion>
+				<ContainerLabels show={show}>
+					{questions.map((item, index) => (
+						<Label
+							key={item.id}
+							selected={selectedQuestion && selectedQuestion.id === item.id}
+							onClick={() => setSelectedQuestion(item)}
+						>
+							<LabelText>
+								Exercício {index + 1}
+								{answeredQuestions.find(
+									(answer) => answer.questionId === item.id
+								) && (
+									<ContainerCheckLabel>
+										<BsCheckLg />
+									</ContainerCheckLabel>
+								)}
+							</LabelText>
+						</Label>
+					))}
+				</ContainerLabels>
+
+				{selectedQuestion && (
+					<Question
+						index={
+							questions.findIndex((item) => item.id === selectedQuestion.id) + 1
+						}
+						totalQuestions={questions.length + 1}
+						question={selectedQuestion}
+						answeredQuestions={answeredQuestions}
+						handleMarkQuestion={handleMarkQuestion}
+						handlePreviousQuestion={
+							questions.findIndex((item) => item.id === selectedQuestion.id) > 0
+								? handlePreviousQuestion
+								: undefined
+						}
+						handleNextQuestion={
+							questions.findIndex((item) => item.id === selectedQuestion.id) <
+							questions.length - 1
+								? handleNextQuestion
+								: undefined
+						}
+					/>
+				)}
+			</section>
+		</Container>
+	);
+};
 
 export default Home;
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,7 +1,111 @@
-import styled from "styled-components";
+import styled, { css } from 'styled-components';
 
 export const Container = styled.div`
-  width: 100vw;
-  height: 100vh;
+	display: flex;
+	padding: 40px 60px;
+	flex-direction: column;
+	max-width: 1366px;
+	margin: 44px auto auto;
 `;
-export const Label = styled.button``;
+
+interface LabelButtonProps {
+	selected?: boolean;
+}
+export const Label = styled.button<LabelButtonProps>`
+	background: ${({ selected, theme }) =>
+		selected ? theme.COLORS.BLUE[700] : 'none'};
+	border-radius: 8px;
+	border: 2px solid ${({ theme }) => theme.COLORS.BLUE[700]};
+	width: 200px;
+	height: 30px;
+	transition: background-color 0.5s;
+	cursor: pointer;
+	font-weight: 600;
+	${({ selected }) =>
+		selected &&
+		css`
+			background: ${({ theme }) => theme.COLORS.BLUE[700]};
+			& > span {
+				color: ${({ theme }) => theme.COLORS.BLUE[900]};
+			}
+			& > span > figure > svg {
+				fill: ${({ theme }) => theme.COLORS.BLUE[900]};
+			}
+		`};
+	&:hover {
+		background: ${({ theme }) => theme.COLORS.BLUE[700]};
+		& > span {
+			color: ${({ theme }) => theme.COLORS.BLUE[900]};
+		}
+		& > svg {
+			fill: ${({ theme }) => theme.COLORS.BLUE[900]};
+		}
+	}
+`;
+interface ContainerLabelsProps {
+	show?: boolean;
+}
+export const ContainerLabels = styled.div<ContainerLabelsProps>`
+	width: 100%;
+	display: flex;
+	gap: 10px;
+	flex-wrap: wrap;
+	overflow: hidden;
+	max-height: ${({ show }) => (show ? '100vh' : '0')};
+	transition: all 0.5s;
+	margin: 10px 0;
+`;
+export const ContainerIcon = styled.span`
+	padding: 5px;
+	background: ${({ theme }) => theme.COLORS.GRAY[200]};
+	align-items: center;
+	justify-content: center;
+	display: flex;
+	border-radius: 50%;
+`;
+export const Header = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	width: 100%;
+`;
+interface ToggleButtonProps {
+	rotate?: boolean;
+}
+export const ToggleButton = styled.button<ToggleButtonProps>`
+	border-radius: 50%;
+	width: 25px;
+	height: 25px;
+	border: none;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	background: ${({ theme }) => theme.COLORS.BLUE[700]};
+	cursor: pointer;
+	transition: 0.5s;
+	${({ rotate }) =>
+		rotate &&
+		css`
+			transform: rotate(-180deg);
+		`}
+	&:hover {
+		background: ${({ theme }) => theme.COLORS.BLUE[400]};
+	}
+`;
+export const HeaderQuestion = styled.header`
+	display: flex;
+	gap: 10px;
+	margin: 14px 0;
+	align-items: center;
+`;
+export const TitleQuestion = styled.h4``;
+export const TitleGroup = styled.h3``;
+export const ContainerCheckLabel = styled.figure`
+	position: absolute;
+	right: -20px;
+	top: 0;
+	bottom: 0;
+`;
+export const LabelText = styled.span`
+	position: relative;
+`;
