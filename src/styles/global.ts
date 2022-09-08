748cbcf2f2922e74eb626efb24523c932a209134
Luiz Claudio <lcluizclaudio96@gmail.com>
2022-09-08 17:01:58 -0300

    define theme

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,17 +1,18 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from 'styled-components';
 
 const Global = createGlobalStyle`
 *{
     padding: 0;
     margin: 0;
     box-sizing: border-box;
+    color: #000;
 }
 html{
     font-family: 'Nunito Sans', sans-serif;
 }
 body{
-    background-color: #191622;
-    color: #f5f8f9;
+    background-color: ${({ theme }) => theme.COLORS.GRAY[100]};
+    
 }
 `;
 export default Global;
